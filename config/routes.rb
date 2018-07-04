d71ea581974d755916cb8c2b72cea8530e4e242c
Neveen-E <nelasar@spartaglobal.com>
2018-07-04 20:31:35 +0100

    controllers and model created

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/new'
+  get 'movies/edit'
+  get 'movies/show'
+  get 'movies/delete'
+  get 'movies/create'
+  get 'movies/update'
+  get 'movie/index'
+  get 'movie/new'
+  get 'movie/edit'
+  get 'movie/show'
+  get 'movie/delete'
+  get 'movie/create'
+  get 'movie/update'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
